36ff3ecaad8deed7c15aed8728dcacd16749649c
vim-key <vmjmjdie7050@163.com>
2021-01-01 14:56:40 +0800

    优化判断子弹是否击中敌方坦克算法 添加判断我方坦克是否与敌方坦克相撞算法 为代码添加注释

--- app/src/main/java/com/zuck/swipe/hitblockrefresh/view/BattleCityView.java
+++ app/src/main/java/com/zuck/swipe/hitblockrefresh/view/BattleCityView.java
@@ -18,35 +18,97 @@ import java.util.Random;
  */
 public class BattleCityView extends FunGameView {
 
-    private static final String tag = "BattleCityView";
-
+    /**
+     * 轨道数量
+     */
     private static int TANK_ROW_NUM = 3;
 
-    static final float TANK_BARREL_RATIO = 1/3.f;
+    /**
+     * 炮管尺寸所在tank尺寸的比率
+     */
+    private static final float TANK_BARREL_RATIO = 1/3.f;
 
-    static final float BULLET_NUM_RATIO = .4f;
+    /**
+     * 默认子弹之间空隙间距
+     */
+    private static final int DEFAULT_BULLET_NUM_SPACING = 360;
 
-    static final int TANK_EXTRA_SPACING = 10;
+    /**
+     * 默认敌方坦克之间间距
+     */
+    private static final int DEFAULT_ENEMY_TANK_NUM_SPACING = 60;
 
+    /**
+     * 表示运行漏掉的敌方坦克总数量 和 升级后消灭坦克总数量的增量
+     */
+    private static final int DEFAULT_TANK_MAGIC_TOTAL_NUM = 10;
+
+    /**
+     * 所有轨道上敌方坦克矩阵集合
+     */
     private SparseArray<Queue<RectF>> eTankSparseArray;
 
+    /**
+     * 屏幕上所有子弹坐标点集合
+     */
     private Queue<Point> mBulletList;
 
+    /**
+     * 击中敌方坦克的子弹坐标点
+     */
     private Point usedBullet;
 
+    /**
+     * 用于随机定位一个轨道下标值
+     */
     private Random random;
 
-    private float heightSize;
-
+    /**
+     * 子弹半径
+     */
     private float bulletRadius;
 
-    private int barrelSize, tankSpaceSize, bulletSpaceSize;
+    /**
+     * 敌方坦克间距、子弹间距
+     */
+    private int enemyTankSpace, bulletSpace;
 
+    /**
+     * 炮筒尺寸
+     */
+    private int barrelSize;
+
+    /**
+     * 敌方坦克速度、子弹速度
+     */
     private int enemySpeed = 2, bulletSpeed = 7;
 
-    private int offsetETankX, offsetMBulletX;
+    /**
+     * 当前前一辆敌方坦克和后一辆已经存在的间距值
+     * 用于确定是否要派出新的一辆敌方坦克
+     */
+    private int offsetETankX;
 
-    private boolean isAdditional, isLaunch;
+    /**
+     * 当前前一颗子弹和后一颗子弹的间距值
+     * 用于确定是否要发射新的一颗子弹
+     */
+    private int  offsetMBulletX;
+
+    /**
+     * 当前漏掉的坦克数量
+     */
+    private int overstepNum;
+
+    /**
+     * 当前难度等级
+     */
+    private int levelNum;
+
+    /**
+     * 当前难度等级内消灭的敌方坦克数量
+     */
+    private int wipeOutNum;
 
     public BattleCityView(Context context) {
         this(context, null);
@@ -64,12 +126,8 @@ public class BattleCityView extends FunGameView {
     protected void initConcreteView() {
         random = new Random();
 
-        heightSize = screenHeight * VIEW_HEIGHT_RATIO;
-        controllerSize = (int) (Math.floor((heightSize - (TANK_ROW_NUM + 1) * DIVIDING_LINE_SIZE) / TANK_ROW_NUM + .5f));
+        controllerSize = (int) (Math.floor((screenHeight * VIEW_HEIGHT_RATIO - (TANK_ROW_NUM + 1) * DIVIDING_LINE_SIZE) / TANK_ROW_NUM + .5f));
         barrelSize = (int) Math.floor(controllerSize * TANK_BARREL_RATIO + .5f);
-        tankSpaceSize = controllerSize + barrelSize + TANK_EXTRA_SPACING;
-        bulletSpaceSize = (int) (screenWidth * BULLET_NUM_RATIO);
-
         bulletRadius = (barrelSize - 2 * DIVIDING_LINE_SIZE) * .5f;
 
         resetConfigParams();
@@ -77,15 +135,23 @@ public class BattleCityView extends FunGameView {
 
     @Override
     protected void drawGame(Canvas canvas) {
-        drawEnemyTank(canvas);
         drawSelfTank(canvas);
-        if (status >=STATUS_GAME_PLAY && status<= STATUS_GAME_FINISHED)
+
+        if (status >=STATUS_GAME_PLAY && status<= STATUS_GAME_FINISHED) {
+            drawEnemyTank(canvas);
             makeBulletPath(canvas);
+        }
     }
 
     @Override
     protected void resetConfigParams() {
         controllerPosition = DIVIDING_LINE_SIZE;
+
+        levelNum = DEFAULT_TANK_MAGIC_TOTAL_NUM;
+        wipeOutNum = 0;
+
+        enemyTankSpace = controllerSize + barrelSize + DEFAULT_ENEMY_TANK_NUM_SPACING;
+        bulletSpace = DEFAULT_BULLET_NUM_SPACING;
 
         eTankSparseArray = new SparseArray<>();
         int option = apperanceOption();
@@ -100,22 +166,28 @@ public class BattleCityView extends FunGameView {
         mBulletList = new LinkedList<>();
     }
 
+    /**
+     * 由index轨道下标从左边起始位置生成一个用于绘制敌方坦克的Rect
+     * @param index 轨道下标
+     * @return 敌方坦克矩阵
+     */
     private RectF generateEnemyTank(int index) {
         float left = - (controllerSize + barrelSize);
-        float top = index * (controllerSize + DIVIDING_LINE_SIZE) + DIVIDING_LINE_SIZE ;
-        return new RectF(left, top, left + controllerSize, top + controllerSize);
+        float top = index * (controllerSize + DIVIDING_LINE_SIZE) + DIVIDING_LINE_SIZE;
+        return new RectF(left, top, left + barrelSize * 2.5f, top + controllerSize);
     }
 
+    /**
+     * 绘制子弹路径
+     * @param canvas 默认画布
+     */
     private void makeBulletPath(Canvas canvas) {
         offsetMBulletX += bulletSpeed;
-        if (offsetMBulletX / bulletSpaceSize == 1) {
-            isLaunch = true;
+        if (offsetMBulletX / bulletSpace == 1) {
             offsetMBulletX = 0;
-        } else {
-            isLaunch = false;
         }
 
-        if (isLaunch) {
+        if (offsetMBulletX == 0) {
             Point bulletPoint = new Point();
             bulletPoint.x = screenWidth - controllerSize - barrelSize;
             bulletPoint.y = (int) (controllerPosition + controllerSize * .5f);
@@ -142,31 +214,101 @@ public class BattleCityView extends FunGameView {
         usedBullet = null;
     }
 
-    private boolean checkWipeOutETank(Point point) {
-        boolean beHit = false;
-        int index = point.y / (getMeasuredHeight() / TANK_ROW_NUM);
+    /**
+     * 由Y坐标获取该坐标所在轨道的下标
+     * @param y 坐标Y值
+     * @return 轨道下标
+     */
+    private int getTrackIndex(int y) {
+        int index = y / (getMeasuredHeight() / TANK_ROW_NUM);
         index = index == TANK_ROW_NUM ? TANK_ROW_NUM - 1 : index;
         index = index == -1 ? 0 : index;
-        Queue<RectF> rectFQueue = eTankSparseArray.get(index);
-        for (RectF rectF : rectFQueue) {
-            if (rectF.contains(point.x, point.y)) { // 击中
-                beHit = true;
-                break;
+        return  index;
+    }
+
+    /**
+     * 判断是否消灭敌方坦克
+     * @param point 单签子弹坐标点
+     * @return 消灭：true, 反之：false
+     */
+    private boolean checkWipeOutETank(Point point) {
+        boolean beHit = false;
+        int trackIndex = getTrackIndex(point.y);
+        RectF rectF = eTankSparseArray.get(trackIndex).peek();
+        if (rectF != null && rectF.contains(point.x, point.y)) { // 击中
+            if (++wipeOutNum == levelNum) {
+                upLevel();
             }
-        }
-        if (beHit) {
-            rectFQueue.poll();
+            eTankSparseArray.get(trackIndex).poll();
+            beHit = true;
         }
         return beHit;
     }
 
+    /**
+     * 难度升级
+     */
+    private void upLevel() {
+        levelNum += DEFAULT_TANK_MAGIC_TOTAL_NUM;
+        enemySpeed++;
+        bulletSpeed += 2;
+        wipeOutNum = 0;
+
+        if (enemyTankSpace > 12)
+        enemyTankSpace -= 12;
+
+        if (bulletSpace > 30)
+        bulletSpace -= 30;
+
+    }
+
+    /**
+     * 绘制子弹
+     * @param canvas 默认画布
+     * @param point 子弹圆心坐标点
+     */
     private void drawBullet(Canvas canvas, Point point) {
         point.x -= bulletSpeed;
         canvas.drawCircle(point.x, point.y, bulletRadius, mPaint);
     }
 
+    /**
+     * 判断我方坦克是否与敌方坦克相撞
+     * @param index 轨道下标
+     * @param selfX 我方坦克所在坐标X值
+     * @param selfY 我方坦克矩阵的top 或者 bottom 值
+     * @return true：相撞，反之：false
+     */
+    private boolean checkTankCrash(int index, float selfX, float selfY) {
+        boolean isCrash = false;
+        RectF rectF = eTankSparseArray.get(index).peek();
+        if (rectF != null && rectF.contains(selfX, selfY)) {
+            isCrash = true;
+        }
+        return isCrash;
+    }
+
+    /**
+     * 绘制我方坦克
+     * @param canvas 默认画布
+     */
     private void drawSelfTank(Canvas canvas) {
-        canvas.drawRect(screenWidth - controllerSize, controllerPosition, screenWidth, controllerPosition + controllerSize, mPaint);
+        boolean isAboveCrash = checkTankCrash(getTrackIndex((int) controllerPosition),
+                screenWidth - controllerSize,
+                controllerPosition);
+        boolean isBelowCrash = checkTankCrash(getTrackIndex((int) (controllerPosition + controllerSize)),
+                screenWidth - controllerSize,
+                controllerPosition + controllerSize);
+
+        if (isAboveCrash || isBelowCrash) {
+            status = STATUS_GAME_OVER;
+        }
+
+        canvas.drawRect(screenWidth - controllerSize,
+                controllerPosition,
+                screenWidth,
+                controllerPosition + controllerSize,
+                mPaint);
         canvas.drawRect(screenWidth - controllerSize - barrelSize,
                 controllerPosition + (controllerSize - barrelSize) * .5f,
                 screenWidth - controllerSize,
@@ -175,41 +317,53 @@ public class BattleCityView extends FunGameView {
     }
 
 
+    /**
+     * 绘制三条轨道上的敌方坦克
+     * @param canvas 默认画布
+     */
     private void drawEnemyTank(Canvas canvas) {
         mPaint.setColor(Color.parseColor("#909090"));
         offsetETankX += enemySpeed;
-        if (offsetETankX / tankSpaceSize == 1) {
-            isAdditional = true;
+        if (offsetETankX / enemyTankSpace == 1) {
             offsetETankX = 0;
-        } else {
-            isAdditional = false;
         }
 
-        boolean isOversetp = false;
+        boolean isOverstep = false;
         int option = apperanceOption();
 
         for (int i = 0; i < TANK_ROW_NUM; i++) {
             Queue<RectF> rectFQueue = eTankSparseArray.get(i);
 
-            if (isAdditional && i == option) {
+            if (offsetETankX == 0 && i == option) {
                 rectFQueue.offer(generateEnemyTank(i));
             }
 
             for (RectF rectF : rectFQueue) {
                 if (rectF.left >= screenWidth) {
-                    isOversetp = true;
+                    isOverstep = true;
+                    if (++overstepNum >= DEFAULT_TANK_MAGIC_TOTAL_NUM) {
+                        status = STATUS_GAME_OVER;
+                        break;
+                    }
                     continue;
                 }
                 drawTank(canvas, rectF);
             }
-            if (isOversetp) {
+
+            if (status == STATUS_GAME_OVER) break;
+            if (isOverstep) {
                 rectFQueue.poll();
-                isOversetp = false;
+                isOverstep = false;
             }
         }
         invalidate();
     }
 
+    /**
+     * 绘制一辆敌方坦克
+     * @param canvas 默认画布
+     * @param rectF 坦克矩阵
+     */
     private void drawTank(Canvas canvas, RectF rectF) {
         rectF.set(rectF.left + enemySpeed, rectF.top, rectF.right + enemySpeed, rectF.bottom);
         canvas.drawRect(rectF, mPaint);
@@ -218,6 +372,10 @@ public class BattleCityView extends FunGameView {
 
     }
 
+    /**
+     * 随机定位一个轨道下标值
+     * @return 轨道下标
+     */
     private int apperanceOption() {
         return random.nextInt(TANK_ROW_NUM);
     }
